ded000437bb8bebcd7cc98257c06d9e3891386cd
Nate Tovo <ntovo@comscore.com>
2013-01-30 13:20:24 -0600

    Adding a unit test for the XmlUtils

---
+++ JenkinsTransport.Tests/XmlUtilsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace JenkinsTransport.Tests
+{
+    [TestFixture]
+    public class XmlUtilsTests
+    {
+        private string BuildServer;
+        private string AuthInfo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            BuildServer = "http://build.office.comscore.com";
+            AuthInfo = Convert.ToBase64String(Encoding.Default.GetBytes(String.Format("{0}:{1}", "cssuser", "c0msc0r3")));
+        }
+
+        [Test]
+        public void TestGetXmlFromUrl()
+        {
+            var xmlDoc = XmlUtils.GetXmlFromUrl(BuildServer + Constants.AllProjects, AuthInfo);
+            Assert.IsNotNull(xmlDoc);
+
+            var jobs = xmlDoc.SelectNodes("/hudson/job");
+            CollectionAssert.IsNotEmpty(jobs);
+        }
+    }
+}
