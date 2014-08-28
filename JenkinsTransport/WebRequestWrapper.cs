f0a120c709cdda71c7f3c3b100731fdad8c0f621
Nathan <locotewz@gmail.com>
2014-08-29 08:38:00 +1200

    Move WebRequestWrapper to seperate file

---
+++ JenkinsTransport/WebRequestWrapper.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Net;
+using JenkinsTransport.Interface;
+
+namespace JenkinsTransport
+{
+    /// <summary>
+    /// Wrapper around an underlying WebRequest instance exposed via IWebRequest
+    /// </summary>
+    public class WebRequestWrapper : IWebRequest
+    {
+        private WebRequest _webRequest;
+
+        public WebHeaderCollection Headers
+        {
+            get { return _webRequest.Headers; }
+            set { _webRequest.Headers = value; }
+        }
+
+        public string Method 
+        {
+            get { return _webRequest.Method; }
+            set { _webRequest.Method = value; } 
+        }
+
+        public WebResponse GetResponse()
+        {
+            return _webRequest.GetResponse();
+        }
+
+        public WebRequestWrapper(string requestUriString)
+        {
+            if (requestUriString == null) 
+                throw new ArgumentNullException("requestUriString");
+
+            _webRequest = WebRequest.Create(requestUriString);
+        }
+
+        public WebRequestWrapper(Uri requestUri)
+        {
+            if (requestUri == null)
+                throw new ArgumentNullException("requestUri");
+
+            _webRequest = WebRequest.Create(requestUri);
+        }
+    }
+}
