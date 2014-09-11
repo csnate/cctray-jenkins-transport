efacd996862bb149348f8cc111865b87c9a152f3
Nathan <locotewz@gmail.com>
2014-09-12 07:29:57 +1200

    Create new integration test project, move integration tests to it

--- JenkinsTransport.IntegrationTests/JenkinsServerManagerTests.cs
+++ JenkinsTransport.IntegrationTests/JenkinsServerManagerTests.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ThoughtWorks.CruiseControl.CCTrayLib.Configuration;
 
-namespace JenkinsTransport.UnitTests.Integration
+namespace JenkinsTransport.IntegrationTests
 {
     [TestClass]
     public class JenkinsServerManagerTests
