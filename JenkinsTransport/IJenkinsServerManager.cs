a87d327056bad85fc197b44ce9bc8637964cedc6
Nathan <njohnstone@boardbooks.com>
2014-11-06 13:39:42 +1300

    Implement an internal inteface IJenkinsServerManager for abstraction of ServerManager from JenkinsTransportExtension

---
+++ JenkinsTransport/IJenkinsServerManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ThoughtWorks.CruiseControl.CCTrayLib.Configuration;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace JenkinsTransport
+{
+    /// <summary>
+    /// Internal interface for abstraction of ServerManager
+    /// </summary>
+    public interface IJenkinsServerManager
+    {
+        Dictionary<string, ProjectStatus> ProjectsAndCurrentStatus { get; }
+        CruiseServerSnapshot GetCruiseServerSnapshot();
+        void SetConfiguration(BuildServer server);
+        CCTrayProject[] GetProjectList();
+        void Initialize(BuildServer server, string session, string settings);
+    }
+}
